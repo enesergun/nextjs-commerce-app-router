5c0f3a5311c2ca2c35a93a3e81dc53077b7efb58
Enes Ergün <enesergun1515@gmail.com>
2024-01-11 23:25:23 +0300

    basket content create

---
+++ app/ui/basketContent/index.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import React from "react";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+
+function ProductCard() {
+  return (
+    <div className="flex justify-between pt-5">
+      <Image
+        src="https://picsum.photos/75/75"
+        width={75}
+        height={75}
+        alt="product"
+        className="rounded-2xl"
+      />
+      <div className="w-2/5">
+        <p>Sweatshirt</p>
+        <small>Sweatshirt</small>
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="text-center">$ 7.00</div>
+        <div className="flex ml-auto h-9 flex-row items-center rounded-full border border-neutral-200 w-24 justify-evenly">
+          <MinusIcon className="h-4 w-4" />
+          <span>1</span>
+          <PlusIcon className="h-4 w-4" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function BasketContent() {
+  return (
+    <div className="mt-3">
+      <h1 className="text-xl">My Cart</h1>
+      <div className="mt-4 flex flex-col gap-10 divide-y">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+      <div className="mt-5 border-t py-4 ">
+        <div className="flex justify-between">
+          <span>Ara Toplam (1 ürün)</span> <span>78,99</span>
+        </div>
+        <div>
+          <button className="w-full mt-4 bg-sky-400 text-white p-3 rounded-2xl">
+            Sepete Git
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BasketContent;
--- app/ui/layout/basket-modal.tsx
+++ app/ui/layout/basket-modal.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import OpenBasket from "./open-basket";
+import BasketContent from "@/app/ui/basketContent";
 
 function BasketModal() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,19 +14,30 @@ function BasketModal() {
         <OpenBasket />
       </button>
 
+      {isOpen && (
+        <div
+          className="fixed w-full h-full top-0 right-0 left-0 bottom-0 bg-black z-20 opacity-50"
+          onClick={() => basketOpen()}
+        ></div>
+      )}
+
       <div
+        /* ref={ref} */
         className={`${
-          isOpen ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"
+          isOpen
+            ? "opacity-100 translate-x-0 overflow-auto"
+            : "opacity-0 translate-x-full"
         } transition-all transform duration-500 ease-in-out bg-white fixed  h-screen w-full top-0 right-0 shadow-xl z-40 p-4 lg:w-[450px] lg:px-6`}
       >
         <button
           aria-label="Close mobile menu"
-          className="flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 float-right"
+          className="flex  h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 float-right"
           onClick={basketOpen}
         >
           <XMarkIcon className="h-4" />
         </button>
         {/* Content of the modal */}
+        <BasketContent />
       </div>
     </div>
   );
