f784f3a7ed0bea3009ece99d655504d2e702536e
Enes Ergün <enesergun1515@gmail.com>
2024-02-11 10:34:51 +0300

    fix #2

--- app/ui/elements/input.tsx
+++ app/ui/elements/input.tsx
@@ -27,7 +27,7 @@ export default function Input({
         id={id}
         name={name}
         className={clsx(
-          "peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2  focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-cyan-950",
+          "peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2  border-t-0 focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-cyan-950",
           error && "border-red-500",
         )}
         placeholder=" "
